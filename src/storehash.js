7fb64cca2ee370aa524abeab672e47a196288519
hanukoon <hwymaster01@gmail.com>
2019-05-10 19:17:13 +0900

    Sd

---
+++ src/storehash.js
@@ -0,0 +1,40 @@
+import Web3 from 'web3';
+//overrides metamask v0.2 for our v 1.0
+const web3 = new Web3(window.web3.currentProvider);
+
+
+//access our local copy to contract deployed on rinkeby testnet
+//use your own contract address
+const address = '0xb84b12e953f5bcf01b05f926728e855f2d4a67a9';
+//use the ABI from your contract
+const abi = [
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getHash",
+    "outputs": [
+      {
+        "name": "x",
+        "type": "string"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "x",
+        "type": "string"
+      }
+    ],
+    "name": "sendHash",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+]
+export default new web3.eth.Contract(abi, address);
